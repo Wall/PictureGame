8be408a0c97520b49a022b71b8cb7fbd7f6c2001
Cameron Lazell <cameron.lazell@lisasoft.com>
2015-01-16 11:28:24 +1100

    Added google play and leaderboards

--- app/src/main/java/com/bluewall/picturegame/MainActivity.java
+++ app/src/main/java/com/bluewall/picturegame/MainActivity.java
@@ -1,39 +1,327 @@
 package com.bluewall.picturegame;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesActivityResultCodes;
+import com.google.android.gms.games.leaderboard.LeaderboardScore;
+import com.google.android.gms.games.leaderboard.LeaderboardVariant;
+import com.google.android.gms.games.leaderboard.Leaderboards;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
+import com.google.android.gms.plus.Plus;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener{
+
+    final static String TAG = "Image Hunt";
+    // Request code used to invoke sign in user interactions.
+    private static final int RC_SIGN_IN = 9001;
+
+    // Client used to interact with Google APIs.
+    private GoogleApiClient mGoogleApiClient;
+
+    // Are we currently resolving a connection failure?
+    private boolean mResolvingConnectionFailure = false;
+
+    // Has the user clicked the sign-in button?
+    private boolean mSignInClicked = false;
+
+    // Set to true to automatically start the sign in flow when the Activity starts.
+    // Set to false to require the user to click the button in order to sign in.
+    private boolean mAutoStartSignInFlow = true;
+
+    @InjectView(R.id.button_sign_in)
+    com.google.android.gms.common.SignInButton logInButton;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+
+        // Create the Google Api Client with access to Plus and Games
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+
+        logInButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+                Log.i(TAG, "Sign-in button clicked");
+                mSignInClicked = true;
+                mGoogleApiClient.connect();
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onActivityResult(int requestCode, int responseCode,
+                                 Intent intent) {
+        super.onActivityResult(requestCode, responseCode, intent);
+
+        switch (requestCode) {
+
+
+            case RC_SIGN_IN:
+                Log.d(TAG, "onActivityResult with requestCode == RC_SIGN_IN, responseCode="
+                        + responseCode + ", intent=" + intent);
+                mSignInClicked = false;
+                mResolvingConnectionFailure = false;
+                if (responseCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                } else {
+                    showActivityResultError(this, requestCode, responseCode, R.string.signin_other_error);
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, responseCode, intent);
+    }
+
+    // Activity just got to the foreground. We switch to the wait screen because we will now
+    // go through the sign-in flow (remember that, yes, every time the Activity comes back to the
+    // foreground we go through the sign-in flow -- but if the user is already authenticated,
+    // this flow simply succeeds and is imperceptible).
+    @Override
+    public void onStart() {
+
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            Log.w(TAG,
+                    "GameHelper: client was already connected on onStart()");
+        } else {
+            Log.d(TAG, "Connecting client.");
+            mGoogleApiClient.connect();
+        }
+        super.onStart();
+    }
+
+       /*
+     * CALLBACKS SECTION. This section shows how we implement the several games
+     * API callbacks.
+     */
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d(TAG, "onConnected() called. Sign in successful!");
+
+        Log.d(TAG, "Sign-in succeeded.");
+
+        int REQUEST_LEADERBOARD = 100;
+
+        Games.Leaderboards.submitScore(mGoogleApiClient, getString(R.string.leaderBoardID), 1337);
+
+        startActivityForResult(Games.Leaderboards.getLeaderboardIntent(mGoogleApiClient,
+                getString(R.string.leaderBoardID)), REQUEST_LEADERBOARD);
+
+
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended() called. Trying to reconnect.");
+        mGoogleApiClient.connect();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed() called, result: " + connectionResult);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (mResolvingConnectionFailure) {
+            Log.d(TAG, "onConnectionFailed() ignoring connection failure; already resolving.");
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (mSignInClicked || mAutoStartSignInFlow) {
+            mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+            mResolvingConnectionFailure = resolveConnectionFailure(this, mGoogleApiClient,
+                    connectionResult, RC_SIGN_IN, getString(R.string.signin_other_error));
+        }
+
+        switchToScreen(R.id.screen_sign_in);
+    }
+
+    /*
+     * GAME LOGIC SECTION. Methods that implement the game's rules.
+     */
+    public void getImage(){
+
+    }
+    public String getAnswer(){
+        String correctAnswer ="";
+
+
+        return correctAnswer;
+    }
+
+    public boolean checkAnswerCorrect(String answer){
+        boolean isCorrect = false;
+
+
+        return isCorrect;
+    }
+
+
+
+
+
+
+    /*
+     * COMMUNICATIONS SECTION. Methods that implement the game's network
+     * protocol.
+     */
+
+
+    // This array lists all the individual screens our game has.
+    final static int[] SCREENS = {
+            R.id.screen_sign_in,
+
+    };
+    int mCurScreen = -1;
+
+    void switchToScreen(int screenId) {
+        // make the requested screen visible; hide all others.
+        for (int id : SCREENS) {
+            findViewById(id).setVisibility(screenId == id ? View.VISIBLE : View.GONE);
+        }
+        mCurScreen = screenId;
+
+
+    }
+
+    void switchToMainScreen() {
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+           // switchToScreen(R.id.screen_main);
+        } else {
+            switchToScreen(R.id.screen_sign_in);
+        }
+    }
+
+
+    //GAME UTILS
+
+    public static Dialog makeSimpleDialog(Activity activity, String text) {
+        return (new AlertDialog.Builder(activity)).setMessage(text)
+                .setNeutralButton(android.R.string.ok, null).create();
+    }
+
+    public static boolean resolveConnectionFailure(Activity activity,
+                                                   GoogleApiClient client, ConnectionResult result, int requestCode,
+                                                   String fallbackErrorMessage) {
+
+        if (result.hasResolution()) {
+            try {
+                result.startResolutionForResult(activity, requestCode);
+                return true;
+            } catch (IntentSender.SendIntentException e) {
+                // The intent was canceled before it was sent.  Return to the default
+                // state and attempt to connect to get an updated ConnectionResult.
+                client.connect();
+                return false;
+            }
+        } else {
+            // not resolvable... so show an error message
+            int errorCode = result.getErrorCode();
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(errorCode,
+                    activity, requestCode);
+            if (dialog != null) {
+                dialog.show();
+            } else {
+                // no built-in dialog: show the fallback error message
+                showAlert(activity, fallbackErrorMessage);
+            }
+            return false;
+        }
+    }
+
+    public static void showAlert(Activity activity, String message) {
+        (new AlertDialog.Builder(activity)).setMessage(message)
+                .setNeutralButton(android.R.string.ok, null).create().show();
+    }
+
+    public static void showActivityResultError(Activity activity, int requestCode, int actResp, int errorDescription) {
+        if (activity == null) {
+            Log.e("BaseGameUtils", "*** No Activity. Can't show failure dialog!");
+            return;
+        }
+        Dialog errorDialog;
+
+        switch (actResp) {
+            case GamesActivityResultCodes.RESULT_APP_MISCONFIGURED:
+                errorDialog = makeSimpleDialog(activity,
+                        activity.getString(R.string.app_misconfigured));
+                break;
+            case GamesActivityResultCodes.RESULT_SIGN_IN_FAILED:
+                errorDialog = makeSimpleDialog(activity,
+                        activity.getString(R.string.sign_in_failed));
+                break;
+            case GamesActivityResultCodes.RESULT_LICENSE_FAILED:
+                errorDialog = makeSimpleDialog(activity,
+                        activity.getString(R.string.license_failed));
+                break;
+            default:
+                // No meaningful Activity response code, so generate default Google
+                // Play services dialog
+                final int errorCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
+                errorDialog = GooglePlayServicesUtil.getErrorDialog(errorCode,
+                        activity, requestCode, null);
+                if (errorDialog == null) {
+                    // get fallback dialog
+                    Log.e("BaseGamesUtils",
+                            "No standard error dialog available. Making fallback dialog.");
+                    errorDialog = makeSimpleDialog(activity, activity.getString(errorDescription));
+                }
+        }
+
+        errorDialog.show();
+    }
+    /*
+     * MISC SECTION. Miscellaneous methods.
+     */
+
+
+    // Sets the flag to keep this screen on. It's recommended to do that during
+    // the
+    // handshake when setting up a game, because if the screen turns off, the
+    // game will be
+    // cancelled.
+    void keepScreenOn() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    // Clears the flag that keeps the screen on.
+    void stopKeepingScreenOn() {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
